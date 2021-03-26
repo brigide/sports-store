33ec5965489027a0e64d196fddcbe481da718b5b
brigide <brigidegd@gmail.com>
2021-03-26 09:53:40 -0300

    initial template

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace sports_store.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
